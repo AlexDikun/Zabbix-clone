626251841c584ffad7c201e85b86a10aae18d6cf
AlexDikun <loban.sasha.97@mail.ru>
2025-03-02 22:21:10 +0300

    git init

---
+++ src/main/java/su/dikunia/zabbix_clone/ZabbixCloneApplication.java
@@ -0,0 +1,13 @@
+package su.dikunia.zabbix_clone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZabbixCloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZabbixCloneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/su/dikunia/zabbix_clone/ZabbixCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package su.dikunia.zabbix_clone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZabbixCloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
