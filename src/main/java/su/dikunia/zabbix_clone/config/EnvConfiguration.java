f442ffddffe3da3a4c9e8107153e61c96319aa7c
AlexDikun <loban.sasha.97@mail.ru>
2025-03-08 19:31:04 +0300

    добавлены: конфигурация для библиотеки dotenv-java и изменен исполнительный файл дял запуска приложения - теперь перед запуском можно считать пермененные окружения и успешно подключится к БД

---
+++ src/main/java/su/dikunia/zabbix_clone/config/EnvConfiguration.java
@@ -0,0 +1,11 @@
+package su.dikunia.zabbix_clone.config;
+
+import io.github.cdimascio.dotenv.Dotenv;
+
+public class EnvConfiguration {
+    public static void loadEnv() {
+        Dotenv dotenv = Dotenv.load();
+        String dbPassword = dotenv.get("SPRING_DATASOURCE_PASSWORD");
+        System.setProperty("spring.datasource.password", dbPassword);
+    } 
+}
