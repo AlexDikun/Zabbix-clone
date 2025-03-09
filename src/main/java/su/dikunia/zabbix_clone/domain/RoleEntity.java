16046d5e059e4c99b23398f0824e92cc050e5572
AlexDikun <loban.sasha.97@mail.ru>
2025-03-09 14:21:33 +0300

    в RoleENTITY добавлено поле хранящее значение дату и времени создания сущности

--- src/main/java/su/dikunia/zabbix_clone/domain/RoleEntity.java
+++ src/main/java/su/dikunia/zabbix_clone/domain/RoleEntity.java
@@ -25,6 +25,9 @@ public class RoleEntity {
     @NotBlank(message = "name must not be blank")
     private String name;
 
+    @Column(name = "created_at", nullable = false)
+    private LocalDateTime createdAt;
+
     public RoleEntity() {}
     
 }
