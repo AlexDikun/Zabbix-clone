126bc96787ff4e20121c4cd343588948723a0597
AlexDikun <loban.sasha.97@mail.ru>
2025-03-22 17:56:02 +0300

    написал классы сервисов для Пользователей и Ролей. Пока там реализовано по одному методу - для созданию сущности через сервисы

---
+++ src/main/java/su/dikunia/zabbix_clone/service/UserService.java
@@ -0,0 +1,41 @@
+package su.dikunia.zabbix_clone.service;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import su.dikunia.zabbix_clone.domain.RoleEntity;
+import su.dikunia.zabbix_clone.domain.UserEntity;
+import su.dikunia.zabbix_clone.repos.UserRepository;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Transactional
+    public UserEntity createUser(final String login, final String password, RoleEntity roleEntity) {
+        Optional<UserEntity> optUser = userRepository.findByLogin(login);
+        UserEntity userEntity;
+
+        if (optUser.isPresent()) 
+            userEntity = optUser.get();
+        else {
+            userEntity = new UserEntity();
+            userEntity.setLogin(login);
+            userEntity.setPassword(passwordEncoder.encode(password));
+            userEntity.setRoleEntity(roleEntity);
+            userEntity = userRepository.save(userEntity);
+        }
+
+        return userEntity;
+    }
+    
+}
