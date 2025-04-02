6b849a2ab3d6051cdf10e0103b4a42b489e3a6d6
AlexDikun <loban.sasha.97@mail.ru>
2025-04-03 00:35:35 +0300

    добавил api-контроллер и метод для создания профиля пользователя администратором. Метод еще не проверен на работоспособность, и нужно починить тесты в связи с изменениями UserService.createUser

---
+++ src/main/java/su/dikunia/zabbix_clone/controllers/api/UserController.java
@@ -0,0 +1,30 @@
+package su.dikunia.zabbix_clone.controllers.api;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import su.dikunia.zabbix_clone.dto.UserDTO;
+import su.dikunia.zabbix_clone.service.UserService;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+
+    public ResponseEntity<UserDTO> createUser(@RequestBody UserDTO userDTO) {
+        System.out.println("Админ создает профиль пользователю!");
+
+        UserDTO createdUser = userService.createUser(userDTO, Optional.empty());
+        return new ResponseEntity<>(createdUser, HttpStatus.CREATED); 
+    }
+    
+}
