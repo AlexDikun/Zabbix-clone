31802454f0b2c996ad9fd37113c0557d6f25c367
AlexDikun <loban.sasha.97@mail.ru>
2025-04-10 00:12:47 +0300

    добавлен UserController и post-метод по созданию аккаунта. Метод доступен только для администраторов

--- src/main/java/su/dikunia/zabbix_clone/controllers/api/UserController.java
+++ src/main/java/su/dikunia/zabbix_clone/controllers/api/UserController.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,6 +23,7 @@ public class UserController {
     private UserService userService;
 
     @PostMapping
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     public ResponseEntity<UserDTO> createUser(@RequestBody UserDTO userDTO) {
         System.out.println("Админ создает профиль пользователю!");
 
