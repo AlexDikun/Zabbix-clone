6cc8c2dcf717401a5ef2f0eff026f01d88f2120f
AlexDikun <loban.sasha.97@mail.ru>
2025-04-10 00:10:36 +0300

    добавлен класс с базовой настройкой безопасности http для приложения. Пока что разрешены все запросы без авторизации

--- src/main/java/su/dikunia/zabbix_clone/config/SecurityConfiguration.java
+++ src/main/java/su/dikunia/zabbix_clone/config/SecurityConfiguration.java
@@ -1,16 +1,33 @@
 package su.dikunia.zabbix_clone.config;
 
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableMethodSecurity(prePostEnabled = true)
 public class SecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+            .csrf(csrf -> csrf.disable()) 
+            .authorizeHttpRequests(authorizeRequests ->
+                authorizeRequests
+                    .anyRequest().permitAll() 
+            );
+
+        return http.build();
+    }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
-    
+
 }
