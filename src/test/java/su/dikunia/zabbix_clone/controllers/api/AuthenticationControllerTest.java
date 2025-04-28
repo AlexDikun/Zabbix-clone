db046a967b56eb80b1867f3bb2550068ee4ca22f
AlexDikun <loban.sasha.97@mail.ru>
2025-04-29 00:29:05 +0300

    добавлены тесты для контроллера аутентификации. два тест-сценария проверяют запрос на получения двух токенов и оба заканчиваются успешно

---
+++ src/test/java/su/dikunia/zabbix_clone/controllers/api/AuthenticationControllerTest.java
@@ -0,0 +1,56 @@
+package su.dikunia.zabbix_clone.controllers.api;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import su.dikunia.zabbix_clone.service.AuthenticationService;
+
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+public class AuthenticationControllerTest {
+
+    @InjectMocks
+    private AuthenticationController authController;
+
+    @Mock
+    private AuthenticationService authenticationService;
+
+    private MockMvc mockMvc;
+
+    @BeforeEach
+    public void setUp() {
+        MockitoAnnotations.openMocks(this);
+        mockMvc = MockMvcBuilders.standaloneSetup(authController).build();
+
+        System.setProperty("jwt.secret", "my_secret_key_here_with_at_least_256_bits");
+    }
+
+    @Test
+    public void testAuthenticate() throws Exception {
+        when(authenticationService.authenticate(anyString(), anyString())).thenReturn("generated_token");
+
+        mockMvc.perform(post("/api/auth/authenticate")
+                .param("login", "testUser@company.ru")
+                .param("password", "password")
+                .contentType(MediaType.APPLICATION_FORM_URLENCODED))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void testRefresh() throws Exception {
+        when(authenticationService.refreshToken(anyString())).thenReturn("new_token");
+
+        mockMvc.perform(post("/api/auth/refresh")
+                .param("token", "old_token")
+                .contentType(MediaType.APPLICATION_FORM_URLENCODED))
+                .andExpect(status().isOk());
+    }
+}
