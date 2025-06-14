2a1000f0970f21103268c4ad8b987f0b1819e36b
AlexDikun <loban.sasha.97@mail.ru>
2025-06-14 07:12:44 +0300

    теперь для создания AuthManager используется HttpSecurity. Это может помочь избежать потенциальных  проблем с зависимостями и созданием бина AuthManager

--- src/main/java/su/dikunia/zabbix_clone/config/SecurityConfiguration.java
+++ src/main/java/su/dikunia/zabbix_clone/config/SecurityConfiguration.java
@@ -3,6 +3,7 @@ package su.dikunia.zabbix_clone.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -43,8 +44,11 @@ public class SecurityConfiguration {
     }
 
     @Bean
-    public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception {
-        return authConfig.getAuthenticationManager();
+    public AuthenticationManager authenticationManager(HttpSecurity http) throws Exception {
+        AuthenticationManagerBuilder authenticationManagerBuilder =
+            http.getSharedObject(AuthenticationManagerBuilder.class);
+        authenticationManagerBuilder.userDetailsService(userDetailsService);
+        return authenticationManagerBuilder.build();
     }
 
     @Bean
