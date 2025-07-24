1a81c180d62682a2cac1cc6bc5769f3da72caf07
AlexDikun <loban.sasha.97@mail.ru>
2025-07-24 22:48:28 +0300

    теперь при post-request /api/auth/authenticate используется DTO а не RequestParams

--- src/main/java/su/dikunia/zabbix_clone/controllers/api/AuthenticationController.java
+++ src/main/java/su/dikunia/zabbix_clone/controllers/api/AuthenticationController.java
@@ -2,6 +2,8 @@ package su.dikunia.zabbix_clone.controllers.api;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import su.dikunia.zabbix_clone.dto.AuthDto;
 import su.dikunia.zabbix_clone.service.AuthenticationService;
 
 
@@ -13,8 +15,8 @@ public class AuthenticationController {
     private AuthenticationService authService;
 
     @PostMapping("/authenticate")
-    public String authenticate(@RequestParam String login, @RequestParam String password) { 
-        return authService.authenticate(login, password);
+    public String authenticate(@RequestBody AuthDto authDto) { 
+        return authService.authenticate(authDto.getLogin(), authDto.getPassword());
     }
 
     @PostMapping("/refresh")
