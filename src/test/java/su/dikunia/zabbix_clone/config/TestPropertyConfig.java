46476c0b31d5d970fdd6a86433b99f2263256fa6
AlexDikun <loban.sasha.97@mail.ru>
2025-08-06 01:03:50 +0300

    добавлены бины DataSource и PlatformTransactionManager, необходимые для управления транзакциями в тестах, помеченных соответствующей аннотацией @Transactional

--- src/test/java/su/dikunia/zabbix_clone/config/TestPropertyConfig.java
+++ src/test/java/su/dikunia/zabbix_clone/config/TestPropertyConfig.java
@@ -1,15 +1,36 @@
 package su.dikunia.zabbix_clone.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
 
 @Configuration
+@EnableTransactionManagement
 public class TestPropertyConfig {
 
     @DynamicPropertySource
     static void registerPgProperties(DynamicPropertyRegistry registry) {
         registry.add("spring.datasource.password", () -> System.getenv("SPRING_DATASOURCE_PASSWORD"));
     }
-    
+
+     @Bean
+    public DataSource dataSource() {
+        return new EmbeddedDatabaseBuilder()
+            .setType(EmbeddedDatabaseType.H2)
+            .build();
+    }
+
+    @Bean
+    public PlatformTransactionManager transactionManager(DataSource dataSource) {
+        return new DataSourceTransactionManager(dataSource);
+    }
 }
+
