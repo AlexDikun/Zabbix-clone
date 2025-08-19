0639175cf4a00de5f2f751995f91001e5a42d4b2
AlexDikun <loban.sasha.97@mail.ru>
2025-08-19 21:34:25 +0300

    добавлен cтатический метод для преобразования UserEntity в UserDTO

--- src/main/java/su/dikunia/zabbix_clone/dto/UserDTO.java
+++ src/main/java/su/dikunia/zabbix_clone/dto/UserDTO.java
@@ -1,6 +1,8 @@
 package su.dikunia.zabbix_clone.dto;
 
 import lombok.Data;
+import su.dikunia.zabbix_clone.domain.UserEntity;
+import su.dikunia.zabbix_clone.enums.RoleName;
 
 @Data
 public class UserDTO {
@@ -11,11 +13,21 @@ public class UserDTO {
 
     private String password;
 
+    private RoleName roleName;
+
     public UserDTO() {}
     
     public UserDTO(String login, String password) {
         this.login = login;
         this.password = password;
+    }
+
+    public static UserDTO fromEntity(UserEntity userEntity) {
+        UserDTO dto = new UserDTO();
+        dto.setId(userEntity.getId());
+        dto.setLogin(userEntity.getLogin());
+        dto.setRoleName(userEntity.getRoleEntity().getName()); 
+        return dto;
     }
     
 }
