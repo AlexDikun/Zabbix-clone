6d60a28b7561359cc6ade454065edaa90018df13
AlexDikun <loban.sasha.97@mail.ru>
2025-08-19 22:27:00 +0300

    реализовал стандартный подход в Spring Security для интеграции кастомных ролей

--- src/main/java/su/dikunia/zabbix_clone/enums/RoleName.java
+++ src/main/java/su/dikunia/zabbix_clone/enums/RoleName.java
@@ -1,5 +1,12 @@
 package su.dikunia.zabbix_clone.enums;
 
-public enum RoleName {
-    STAFF, MODER, ADMIN
+import org.springframework.security.core.GrantedAuthority;
+
+public enum RoleName implements GrantedAuthority {
+    STAFF, MODER, ADMIN;
+
+    @Override
+    public String getAuthority() {
+        return "ROLE_" + name();
+    }
 }
