c5b93389fe43425c4ffb906bb323e2f2d0011999
AlexDikun <loban.sasha.97@mail.ru>
2025-08-20 00:26:48 +0300

    Используя @JsonIgnore для геттера пароля, не возвращаем его в Json. Используя @JsonProperty для сеттера пароля разрешаем устанавливать его из JSON

--- src/main/java/su/dikunia/zabbix_clone/dto/UserDTO.java
+++ src/main/java/su/dikunia/zabbix_clone/dto/UserDTO.java
@@ -1,6 +1,7 @@
 package su.dikunia.zabbix_clone.dto;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 import lombok.Data;
 import su.dikunia.zabbix_clone.domain.UserEntity;
@@ -12,10 +13,18 @@ public class UserDTO {
     private Long id;
 
     private String login;
-    @JsonIgnore
     private String password;
-
     private RoleName roleName;
+
+    @JsonIgnore
+    public String getPassword() {
+        return password;
+    }
+
+    @JsonProperty
+    public void setPassword(String password) {
+        this.password = password;
+    }
 
     public UserDTO() {}
     
