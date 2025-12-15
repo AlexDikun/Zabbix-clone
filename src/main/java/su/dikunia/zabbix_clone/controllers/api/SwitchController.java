51f1a2899cfbe17b82c115ff65254463d9560ff7
AlexDikun <loban.sasha.97@mail.ru>
2025-12-15 23:22:10 +0300

    добавил в SwitchController метод-обработчик исключения SwitchAlreadyExistsException

--- src/main/java/su/dikunia/zabbix_clone/controllers/api/SwitchController.java
+++ src/main/java/su/dikunia/zabbix_clone/controllers/api/SwitchController.java
@@ -4,12 +4,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import su.dikunia.zabbix_clone.dto.SwitchCreateDTO;
+import su.dikunia.zabbix_clone.exceptions.SwitchAlreadyExistsException;
 import su.dikunia.zabbix_clone.service.SwitchService;
 
 @RestController
@@ -26,6 +28,13 @@ public class SwitchController {
 
         SwitchCreateDTO createdSwitch = switchService.createSwitch(request);
         return new ResponseEntity<>(createdSwitch, HttpStatus.CREATED);
+    }
+
+    @ExceptionHandler(SwitchAlreadyExistsException.class)
+    public ResponseEntity<String> handleSwitchAlreadyExists(SwitchAlreadyExistsException ex) {
+        return ResponseEntity
+            .badRequest()
+            .body(ex.getMessage());
     }
     
 }
