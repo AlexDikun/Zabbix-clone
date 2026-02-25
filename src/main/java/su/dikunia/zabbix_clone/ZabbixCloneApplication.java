f78eb4ed054adf4c7d5fe174311ec0883f3ac057
AlexDikun <loban.sasha.97@mail.ru>
2026-02-25 19:42:06 +0300

    подключил поддержку планировщиков

--- src/main/java/su/dikunia/zabbix_clone/ZabbixCloneApplication.java
+++ src/main/java/su/dikunia/zabbix_clone/ZabbixCloneApplication.java
@@ -2,10 +2,12 @@ package su.dikunia.zabbix_clone;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 import su.dikunia.zabbix_clone.config.EnvConfiguration;
 
 @SpringBootApplication
+@EnableScheduling 
 public class ZabbixCloneApplication {
 	public static void main(String[] args) {
 		EnvConfiguration.loadEnv();
